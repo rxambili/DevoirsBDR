56fa3f29422f9061000ca8b5e50125fdce7fc50e
rxambili <r.xambili@gmail.com>
2018-11-16 21:02:56 -0500

    Add gestion des dégâts (tout le monde attaque tout le monde)

--- Devoir2/src/Combat1.scala
+++ Devoir2/src/Combat1.scala
@@ -28,20 +28,19 @@ package Combat1 {
   import org.apache.spark.graphx.{Edge, EdgeContext, Graph, _}
 
   import scala.util.Random
+  import scala.util.matching.Regex
 
   //tiebreak value is generated randomly before every graph iteration
-  class node(val id: Int, val name: String, val hp: Int = 1, val armor: Int, val regen: Int, val atk: Int, val death: Boolean = false) extends Serializable {
+  class node(val id: Int, val name: String, val hp: Int = 1, val armor: Int, val regen: Int, val atk: Int, val damages: String, val death: Boolean = false) extends Serializable {
     override def toString: String = s"id : $id name : $name hp : $hp dead? : $death"
   }
 
   class Combat1 extends Serializable {
-    def getChromaticNumber(g: Graph[node, String]): Int = {
-      val aa = g.vertices.collect()
-      var maxColor = 0
-      for (i <- aa) {
-        if (i._2.color > maxColor) maxColor = i._2.color
-      }
-      maxColor
+
+    def parseDamages(damages : String): Int = {
+      val pattern = "([0-9]+)d([0-9]+) . ([0-9]+)".r
+      val pattern(nbDes, nbFaces, constant) = damages
+      return getJetDes(nbFaces.toInt, nbDes.toInt) + constant.toInt
     }
 
     def getJetDes(nbFaces: Int, nbDes: Int): Int = {
@@ -54,23 +53,42 @@ package Combat1 {
       return res
     }
 
-    def sendAtkValue(ctx: EdgeContext[node, String, Long]): Unit = {
+    def sendDamageValue(ctx: EdgeContext[node, String, Int]): Unit = {
       if (ctx.srcAttr.death == false && ctx.dstAttr.death == false) {
-        ctx.sendToDst(ctx.srcAttr.tiebreakValue)
-        ctx.sendToSrc(ctx.dstAttr.tiebreakValue)
+        var damageToDst = 0
+        if (getJetDes(20,1) + ctx.srcAttr.atk >=  ctx.dstAttr.armor) {
+          damageToDst = parseDamages(ctx.srcAttr.damages)
+        }
+
+        var damageToSrc = 0;
+        if (getJetDes(20,1) + ctx.srcAttr.atk >=  ctx.dstAttr.armor) {
+          damageToSrc = parseDamages(ctx.dstAttr.damages)
+        }
+
+        ctx.sendToDst(damageToDst)
+        ctx.sendToSrc(damageToSrc)
       }
     }
 
-    def sendHpValue(ctx: EdgeContext[node, String, Long]): Unit = {
+    def sendHpValue(ctx: EdgeContext[node, String, node]): Unit = {
       if (ctx.srcAttr.death == false && ctx.dstAttr.death == false) {
-        ctx.sendToDst(ctx.srcAttr.hp)
-        ctx.sendToSrc(ctx.dstAttr.hp)
+        ctx.sendToDst(ctx.srcAttr)
+        ctx.sendToSrc(ctx.dstAttr)
       }
     }
 
-    def selectBestTarget(hp: Int, id2: Int): Long = {
-      if (id1 < id2) id1
-      else id2
+    def selectBestTarget(target1: node, target2: node): node = {
+      if (target1.hp < target2.hp) target1
+      else target2
+    }
+
+    def sumTotalDamages(damage1: Int, damage2: Int): Int = {
+      return damage1 + damage2
+    }
+
+    def takeDamages(vid: VertexId, sommet: node, totalDamages: Int): node = {
+      val newHp = sommet.hp - totalDamages
+      return new node(sommet.id, sommet.name, newHp, sommet.armor, sommet.regen, sommet.atk, sommet.damages, death = newHp <= 0)
     }
 
     def execute(g: Graph[node, String], maxIterations: Int, sc: SparkContext): Graph[node, String] = {
@@ -85,16 +103,27 @@ package Combat1 {
           counter += 1
           if (counter == maxIterations) return
 
-          val messages = myGraph.aggregateMessages[Long](
-            sendHpValue,
-            selectBestTarget,
+          val messages = myGraph.aggregateMessages[Int](
+            sendDamageValue,
+            sumTotalDamages,
             fields //use an optimized join strategy (we don't need the edge attribute)
           )
 
           if (messages.isEmpty()) return
 
+          //Ignorez : Code de debug
+          var printedMessages = messages.collect()
+          printedMessages = printedMessages.sortBy(_._1)
+          printedMessages.foreach(
+            elem => println(elem._2)
+          )
+
           myGraph = myGraph.joinVertices(messages)(
-            (vid, sommet, bestId) => increaseColor(vid, sommet, bestId))
+            (vid, sommet, totalDamages) => takeDamages(vid, sommet, totalDamages))
+
+          // Regen
+          myGraph = myGraph.mapVertices(
+            (vid, sommet) => new node(sommet.id, sommet.name, sommet.hp + sommet.regen, sommet.armor, sommet.regen, sommet.atk, sommet.damages, sommet.death))
 
           //Ignorez : Code de debug
           var printedGraph = myGraph.vertices.collect()
@@ -118,21 +147,21 @@ package Combat1 {
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
     var myVertices = sc.makeRDD(Array(
-      (1L, new node(id = 1, name = "Solar", hp = 363, armor = 44, regen = 15, atk = 35, death = false)),
-      (2L, new node(id = 2, name = "Worg Rider 1", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (3L, new node(id = 3, name = "Worg Rider 2", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (4L, new node(id = 4, name = "Worg Rider 3", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (5L, new node(id = 5, name = "Worg Rider 4", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (6L, new node(id = 6, name = "Worg Rider 5", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (7L, new node(id = 7, name = "Worg Rider 6", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (8L, new node(id = 8, name = "Worg Rider 7", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (9L, new node(id = 9, name = "Worg Rider 8", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (10L, new node(id = 10, name = "Worg Rider 9", hp = 13, armor = 18, regen = 0, atk = 6, death = false)),
-      (11L, new node(id = 11, name = "Brutal Warlord", hp = 141, armor = 27, regen = 0, atk = 20, death = false)),
-      (12L, new node(id = 12, name = "Barbare Orc 1", hp = 142, armor = 17, regen = 0, atk = 19, death = false)),
-      (13L, new node(id = 13, name = "Barbare Orc 2", hp = 142, armor = 17, regen = 0, atk = 19, death = false)),
-      (14L, new node(id = 14, name = "Barbare Orc 3", hp = 142, armor = 17, regen = 0, atk = 19, death = false)),
-      (15L, new node(id = 15, name = "Barbare Orc 4", hp = 142, armor = 17, regen = 0, atk = 19, death = false))))
+      (1L, new node(id = 1, name = "Solar", hp = 363, armor = 44, regen = 15, atk = 35, damages = "3d6 + 18", death = false)),
+      (2L, new node(id = 2, name = "Worg Rider 1", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (3L, new node(id = 3, name = "Worg Rider 2", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (4L, new node(id = 4, name = "Worg Rider 3", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (5L, new node(id = 5, name = "Worg Rider 4", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (6L, new node(id = 6, name = "Worg Rider 5", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (7L, new node(id = 7, name = "Worg Rider 6", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (8L, new node(id = 8, name = "Worg Rider 7", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (9L, new node(id = 9, name = "Worg Rider 8", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (10L, new node(id = 10, name = "Worg Rider 9", hp = 13, armor = 18, regen = 0, atk = 6, damages = "1d8 + 2", death = false)),
+      (11L, new node(id = 11, name = "Brutal Warlord", hp = 141, armor = 27, regen = 0, atk = 20, damages = "1d8 + 10", death = false)),
+      (12L, new node(id = 12, name = "Barbare Orc 1", hp = 142, armor = 17, regen = 0, atk = 19, damages = "1d8 + 10", death = false)),
+      (13L, new node(id = 13, name = "Barbare Orc 2", hp = 142, armor = 17, regen = 0, atk = 19, damages = "1d8 + 10", death = false)),
+      (14L, new node(id = 14, name = "Barbare Orc 3", hp = 142, armor = 17, regen = 0, atk = 19, damages = "1d8 + 10", death = false)),
+      (15L, new node(id = 15, name = "Barbare Orc 4", hp = 142, armor = 17, regen = 0, atk = 19, damages = "1d8 + 10", death = false))))
 
     var myEdges = sc.makeRDD(Array(
       Edge(1L, 2L, "1"),
@@ -154,6 +183,6 @@ package Combat1 {
     var myGraph = Graph(myVertices, myEdges)
     val algoCombat1 = new Combat1()
     val res = algoCombat1.execute(myGraph, 20, sc)
-    println("\nNombre de couleur trouvées: " + algoCombat1.getChromaticNumber(res))
+    //println("\nNombre de couleur trouvées: " + algoCombat1.getChromaticNumber(res))
   }
 }
